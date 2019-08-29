368008260bdafbc06ad2abf9152a7abbeaa142c8
AJ <aj@Alexandras-MacBook-Pro-3.local>
2019-08-29 09:46:31 -0400

    Updates

---
+++ precache-manifest.3152d305bfdd6c07be2352d89d68dccc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c44f87215c3f24ca2e17cb64a80a175",
+    "url": "/restaurant_finder/index.html"
+  },
+  {
+    "revision": "fef7b1683028c781b088",
+    "url": "/restaurant_finder/static/css/main.79035d10.chunk.css"
+  },
+  {
+    "revision": "d64ebaf9b87a4cb3b444",
+    "url": "/restaurant_finder/static/js/2.855cee18.chunk.js"
+  },
+  {
+    "revision": "fef7b1683028c781b088",
+    "url": "/restaurant_finder/static/js/main.22e118b0.chunk.js"
+  },
+  {
+    "revision": "b58ae038048cb2094ecc",
+    "url": "/restaurant_finder/static/js/runtime~main.8bbb4c67.js"
+  }
+]);
